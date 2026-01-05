a27f608985d45500d5036ec0988c4a39a89d0cdc
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-01-05 03:30:59 -1000

    Create __init__.py

---
+++ python/authority/__init__.py
@@ -0,0 +1,12 @@
+"""
+APE Authority Module
+
+Contains authority token management and lifecycle.
+"""
+
+from ape.authority.manager import AuthorityToken, AuthorityManager
+
+__all__ = [
+    "AuthorityToken",
+    "AuthorityManager",
+]
