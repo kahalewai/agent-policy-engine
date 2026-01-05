41c8330bffd1aa571fa896ded790ff4da07924bc
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-01-05 03:35:40 -1000

    Create __init__.py

---
+++ python/intent/__init__.py
@@ -0,0 +1,12 @@
+"""
+APE Intent Module
+
+Contains intent management and validation.
+"""
+
+from ape.intent.manager import Intent, IntentManager
+
+__all__ = [
+    "Intent",
+    "IntentManager",
+]
