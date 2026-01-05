47ce51af0d17b04c2093b9d4abf4d36b0f847a78
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-01-05 03:29:29 -1000

    Create __init__.py

---
+++ python/enforcement/__init__.py
@@ -0,0 +1,12 @@
+"""
+APE Enforcement Module
+
+Contains the enforcement gate for tool execution.
+"""
+
+from ape.enforcement.gate import EnforcementGate, ExecutionResult
+
+__all__ = [
+    "EnforcementGate",
+    "ExecutionResult",
+]
