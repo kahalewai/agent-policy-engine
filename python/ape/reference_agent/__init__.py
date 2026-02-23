18bb338f36872cf2e7a06b89c046fe3f52d3d2db
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-02-22 16:58:41 -1000

    Create __init__.py

---
+++ python/ape/reference_agent/__init__.py
@@ -0,0 +1,13 @@
+"""
+APE Reference Agent Module
+
+Contains the reference agent implementation.
+"""
+
+from ape.reference_agent.agent import ReferenceAgent, AgentResult, create_simple_agent
+
+__all__ = [
+    "ReferenceAgent",
+    "AgentResult",
+    "create_simple_agent",
+]
