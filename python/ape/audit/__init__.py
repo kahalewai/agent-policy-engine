2cdecfdbddf2d8e9f37bc28c6d219009200f05a4
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-02-22 16:37:59 -1000

    Create __init__.py

---
+++ python/ape/audit/__init__.py
@@ -0,0 +1,13 @@
+"""
+APE Audit Module
+
+Contains audit logging functionality.
+"""
+
+from ape.audit.logger import AuditLogger, AuditEvent, AuditEventType
+
+__all__ = [
+    "AuditLogger",
+    "AuditEvent",
+    "AuditEventType",
+]
