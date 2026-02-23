922ac23c0f34d8f05e473d1896b2489d14fb845a
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-02-22 16:39:44 -1000

    Update action.py

--- python/ape/action/action.py
+++ python/ape/action/action.py
@@ -1,7 +1,7 @@
 """
 APE Action Module
 
-Per architecture spec (§5.3 Action):
+Action:
 - An Action is the smallest unit of authority
 - Actions are explicit, comparable, auditable, deterministically bound to authority
 - Actions are matchable by policy
