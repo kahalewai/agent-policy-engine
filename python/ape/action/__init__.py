78dcdbab94b0592906e0886ea828b8a235cc9413
Shawn Reilly <shawn.k.reilly@gmail.com>
2026-02-22 16:34:44 -1000

    Create __init__.py

---
+++ python/ape/action/__init__.py
@@ -0,0 +1,12 @@
+"""
+APE Action Module
+
+Contains the Action class representing the smallest unit of authority.
+"""
+
+from ape.action.action import Action, validate_action_data
+
+__all__ = [
+    "Action",
+    "validate_action_data",
+]
